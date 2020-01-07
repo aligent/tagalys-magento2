8ed7bff6497b385e3de28e5061de47e28c34f3c9
William Tran <william@aligent.com.au>
2020-01-07 10:57:12 +1030

    Move classes into sub-folder to prevent Magento error warning at setup:di:compile step

--- Controller/Product/Details.php
+++ Controller/Product/Details.php
@@ -4,7 +4,8 @@ use Magento\Framework\App\CsrfAwareActionInterface;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\App\Request\InvalidRequestException;
 
-if (interface_exists("Magento\Framework\App\CsrfAwareActionInterface"))
-    include __DIR__ . "/Details.m230.php";
-else
-    include __DIR__ . "/Details.m220.php";
+if (interface_exists("Magento\Framework\App\CsrfAwareActionInterface")) {
+    include __DIR__ . DIRECTORY_SEPARATOR . 'M220' . DIRECTORY_SEPARATOR . "Details.php";
+} else {
+    include __DIR__ . DIRECTORY_SEPARATOR . 'M230' . DIRECTORY_SEPARATOR . "Details.php";
+}
